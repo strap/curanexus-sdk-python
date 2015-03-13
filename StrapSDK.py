4d78512beb11f8f039d3f3b1aa7b3dda7753f4e2
Marcelle Bonterre <marcelle.bonterre@gmail.com>
2015-03-13 15:15:15 -0400

    added error checking for failed discovery

--- StrapSDK.py
+++ StrapSDK.py
@@ -5,20 +5,33 @@ from Resource import Resource
 class StrapSDK(object):
 
 	def __init__(self, token):
+		self.error = None
 		self._discoveryURL = "https://api2.straphq.com/discover"
 		self._token = token
 		self._resources = self._discover()
-		self._error = ""
 
 	def _discover(self):
 		# get resource list with descriptors
 		r = requests.get(self._discoveryURL, headers={'x-auth-token': self._token})
+		res = r.json()
+		
+		# return early if discovery failed
+		if "success" in res:
+			if res["success"] == False:
+				self.error = res
+				return None
 
 		# create resource objects out of each service url
-		services, resources = r.json(), {}
+		services, resources = res, {}
 		for s in services:
 			resources[s] = Resource(s, services[s], self._token)
 		return resources
+
+	def hasError(self):
+		rv = False
+		if self.error:
+			rv = True
+		return rv
 
 	def getActivity(self, params=None):
 		if params == None:
