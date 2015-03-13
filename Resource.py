47358f6e0a97cde699ac7e76e3cead2d6fe71975
Marcelle Bonterre <marcelle.bonterre@gmail.com>
2015-03-13 15:15:30 -0400

    filter out invalid params

--- Resource.py
+++ Resource.py
@@ -2,6 +2,19 @@
 import requests
 import re
 
+class StrapResponse(object):
+
+	def __init__(self,data=None,error=None):
+		self.data = data
+		self.error = error
+
+	def setData(self,data):
+		self.data = data
+
+	def setError(self,err):
+		self.error = error
+
+
 class Resource(object):
 
 	def __init__(self,name,json,token):
@@ -10,8 +23,7 @@ class Resource(object):
 		self._uri = json["uri"];
 		self._method = json["method"]
 		self._description = json["description"];
-		self._required = []
-		self._optional = json["optional"]
+		self._optional = set(json["optional"])
 
 	def call(self,method="get",params=None):
 		if params == None:
@@ -25,18 +37,21 @@ class Resource(object):
 				url = re.sub('\{'+m+'\}',params[m],url)
 				# remove url param from rest of params 
 				params.pop(m,None)
-		
-		# pass remaining params as querystring
-		r = requests.get(url,params=params,headers={'x-auth-token':self._token})
-		return package_error_with_data(r.json())
 
-	def package_error_with_data(res):
-		rv = {
-			data : res,
-			error : ""
-		}
+		# only pass params if on optional list
+		valid = filter(lambda p: p in self._optional,params)
+		
+		# pass remaining valid params as querystring
+		r = requests.get(url,params=valid,headers={'x-auth-token':self._token})
+
+		return self._package_error_with_data(r.json())
+
+	def _package_error_with_data(self,res=None):
+		if res == None:
+			res = ""
+
+		rv = StrapResponse(res,None)
 		if "success" in res:
-			if res["success"] == false:
-				rv["data"] = ""
-				rv["error"] = res
+			if res["success"] == False:
+				rv.setError(res)
 		return rv
